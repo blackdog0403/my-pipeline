390888accec0c85291c234d78270babdf7ec3afd
Kwangyoung Kim <blackdog0403@gmail.com>
2021-09-13 19:06:09 +0900

    initial commit

---
+++ bin/my-pipeline.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { MyPipelineStack } from '../lib/my-pipeline-stack';
+
+const app = new cdk.App();
+new MyPipelineStack(app, 'MyPipelineStack', {
+  env: {
+    account: '245775375184',
+    region: 'us-west-2',
+  }
+});
+
+app.synth();
